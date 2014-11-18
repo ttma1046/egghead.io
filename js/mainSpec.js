d7a9c858175110d3e70e5d1cc1cbce256b32607e
Chandler <maybe1046@hotmail.com>
2014-11-18 20:53:22 +1100

    07 Testing Overview.

--- js/mainSpec.js
+++ js/mainSpec.js
@@ -4,6 +4,7 @@ describe('filter', function() {
 	describe('reverse', function () {
 		it('should reverse a string', inject(function (reverseFilter) {
 			expect(reverseFilter('ABCD')).toEqual('DCBA');
+			expect(reverseFilter('John')).toEqual('nhoJ');
 		}))	
 	})
 })
