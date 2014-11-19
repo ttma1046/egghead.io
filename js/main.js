d830034d2cf45c154728b2bd3c58cc79aead904f
Chandler <maybe1046@hotmail.com>
2014-11-19 18:10:09 +1100

    8,9 ngFilters

--- js/main.js
+++ js/main.js
@@ -1,22 +1,84 @@
 var myApp = angular.module('myApp',[])
-myApp.factory('Data', function(){
-	return {message:"I'm data from a service."}
+
+myApp.factory('Avengers', function(){
+	var Avengers = {}; 
+
+	Avengers.cast = [
+    {
+      name: 'Robert Downey Jr.',
+      character: 'Tony Stark / Iron Man'
+    },
+    {
+      name: 'Chris Evans',
+      character: 'Steve Rogers / Captain America'
+    },
+    {
+      name: 'Mark Ruffalo',
+      character: 'Bruce Banner / The Hulk'
+    },
+    {
+      name: 'Chris Hemsworth',
+      character: 'Thor'
+    },
+    {
+      name: 'Scarlett Johansson',
+      character: 'Natasha Romanoff / Black Widow'
+    },
+    {
+      name: 'Jeremy Renner',
+      character: 'Clint Barton / Hawkeye'
+    },
+    {
+      name: 'Tom Hiddleston',
+      character: 'Loki'
+    },
+    {
+      name: 'Clark Gregg',
+      character: 'Agent Phil Coulson'
+    },
+    {
+      name: 'Cobie Smulders',
+      character: 'Agent Maria Hill'
+    },
+    {
+      name: 'Stellan Skarsg氓rd',
+      character: 'Selvig'
+    },
+    {
+      name: 'Samuel L. Jackson',
+      character: 'Nick Fury'
+    },
+    {
+      name: 'Gwyneth Paltrow',
+      character: 'Pepper Potts'
+    },
+    {
+      name: 'Paul Bettany',
+      character: 'Jarvis (voice)'
+    },
+    {
+      name: 'Alexis Denisof',
+      character: 'The Other'
+    },
+    {
+      name: 'Tina Benko',
+      character: 'NASA Scientist'
+    }
+  ];
+
+	return Avengers;
 });
 
-myApp.filter('reverse', function(Data) {
-	return function(text){
-		return text.split("").reverse().join("") + Data.message; 
+function AvengersCtrl($scope, Avengers) {
+	$scope.avengers = Avengers;
+}
+
+
+var app = angular.module("superhero", [])
+
+app.directive("superman", function(){
+	return {
+		restrict: "E",
+		template: "<div>Here I am to save the day.</div>"
 	};
-});
-
-function FirstCtrl($scope, Data) {
-	$scope.data = Data;
-}
-
-function SecondCtrl($scope, Data) {
-	$scope.data = Data;
-
-	$scope.reversedMessage = function (message) {
-		return message.split("").reverse().join(""); 
-	}
-}
+})
