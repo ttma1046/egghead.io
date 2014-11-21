d9480517376ce0065d9b52675c6b6d1a2844b69d
Chandler <maybe1046@hotmail.com>
2014-11-21 17:54:40 +1100

    Directive: Class

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ var app = angular.module("superhero", [])
 
 app.directive("superman", function(){
 	return {
-		restrict: "A",
+		restrict: "C",
 		link: function() {
       alert("I am working!")
     }
