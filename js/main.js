ac018bf69b22da965dcc1886a2bbd99ec233c332
Chandler <maybe1046@hotmail.com>
2014-11-26 08:16:18 +1100

    11. Directive Restrictions

--- js/main.js
+++ js/main.js
@@ -78,9 +78,18 @@ var app = angular.module("superhero", [])
 
 app.directive("superman", function(){
 	return {
-		restrict: "M",
+		restrict: "A",
 		link: function() {
-      alert("I am working!")
+      alert("I am working stronger!")
     }
 	};
+})
+
+app.directive("flash", function(){
+  return {
+    restrict: "A",
+    link: function() {
+      alert("I am working faster!")
+    }
+  };
 })
