d288e1914810a50d03494c54bf338cae6a58e5a5
Chandler <maybe1046@hotmail.com>
2014-11-26 17:08:17 +1100

    12. Basic Behaviors part 2

--- js/main.js
+++ js/main.js
@@ -99,7 +99,19 @@ var app = angular.module("behaviorApp", []);
 app.directive("enter", function () {
   return function (scope, element) {
     element.bind("mouseenter", function() {
-      console.log("I'm inside of you.")
+      console.log("I'm inside of you.");
     })
   }    
-})
+});
+
+
+app.directive("leave", function () {
+  return function (scope, element) {
+    element.bind("mouseleave", function() {
+      console.log("I'm leaving on a jet plane.");
+    })
+  }    
+});
+
+
+
