8914debc3184c929c5b7c4c40487986322859f6a
Chandler <maybe1046@hotmail.com>
2014-11-26 18:36:39 +1100

    13 Useful Behaviors

--- js/main.js
+++ js/main.js
@@ -97,18 +97,18 @@ app.directive("flash", function(){
 var app = angular.module("behaviorApp", []);
 
 app.directive("enter", function () {
-  return function (scope, element) {
+  return function (scope, element, attrs) {
     element.bind("mouseenter", function() {
-      console.log("I'm inside of you.");
+      element.addClass(attrs.enter);
     })
   }    
 });
 
 
 app.directive("leave", function () {
-  return function (scope, element) {
+  return function (scope, element, attrs) {
     element.bind("mouseleave", function() {
-      console.log("I'm leaving on a jet plane.");
+      element.removeClass(attrs.enter);
     })
   }    
 });
