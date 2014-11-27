27d48d13233b1fce185f3df01e9a1be2aa9ccb76
Chandler <maybe1046@hotmail.com>
2014-11-28 08:24:52 +1100

    14 Directives talking to Controllers

--- js/main.js
+++ js/main.js
@@ -113,5 +113,43 @@ app.directive("leave", function () {
   }    
 });
 
+var app = angular.module("twitterApp", []);
+ 
+app.controller("AppCtrl", function($scope) {
+  $scope.loadMoreTweets = function () {
+    alert("Loading tweets!");
+  };
+ }); 
+
+ app.directive("enter", function() {
+  return function(scope, element, attrs) {
+    element.bind("mouseenter", function(){
+      scope.loadMoreTweets();
+    })
+  }
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
