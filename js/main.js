dbf9e16f2fa97bfbe6def3b8921552aed5b2a6c6
Chandler <maybe1046@hotmail.com>
2014-11-30 17:04:27 +1100

    15. Directive to Directive Communication

--- js/main.js
+++ js/main.js
@@ -124,15 +124,71 @@ app.controller("AppCtrl", function($scope) {
  app.directive("enter", function() {
   return function(scope, element, attrs) {
     element.bind("mouseenter", function(){
-      scope.loadMoreTweets();
+      scope.$apply(attrs.enter);
     })
   }
  })
 
+var app = angular.module('superApp',[]);
 
+app.directive("superhero", function () {
+  return {
+    // restrict: "E",
+    scope: {},
+    controller:function($scope){
+      $scope.abilities = [];
 
+      this.addStrength = function() {
+        $scope.abilities.push("strength");
+      };
 
+      this.addSpeed = function () {
+        $scope.abilities.push("speed");
+      };
 
+      this.addFlight = function () {
+        $scope.abilities.push("Flight");
+      };      
+    },
+
+    link: function(scope, element){
+      element.addClass("button");
+      element.bind("mouseenter", function() {
+          console.log(scope.abilities);
+      })
+    }
+  }
+})
+
+app.directive("strength", function () {
+  return {
+    require:"superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addStrength();
+        
+    }
+  };
+})
+
+app.directive("flight", function () {
+  return {
+    require:"superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addFlight();
+        
+    }
+  };
+})
+
+app.directive("speed", function () {
+  return {
+    require:"superhero",
+    link: function(scope, element, attrs, superheroCtrl){
+      superheroCtrl.addSpeed();
+        
+    }
+  };
+})
 
 
 
