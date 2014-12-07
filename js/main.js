00530f5d60f374c4140fe72d3593057128084a04
Chandler <maybe1046@hotmail.com>
2014-12-07 20:08:28 +1100

    36. Resolve conventions

--- js/main.js
+++ js/main.js
@@ -393,25 +393,42 @@ app.config(function($routeProvider) {
       templateUrl: "view/app.html",
       controller: "AppCtrl",
       resolve: {
-        app: function($q, $timeout) {
-          var defer = $q.defer();
-          
-          $timeout(function () {
-            defer.resolve();
-          }, 2000);
-
-          return defer.promise;
-        }
+        loadData: appCtrl.loadData,
+        prepData: appCtrl.prepData  
       }
 
     })
 });
 
-app.controller('AppCtrl', function($scope){
+var appCtrl = app.controller('AppCtrl', function($scope, $route){
+  console.log($route);
+
   $scope.model = {
     message: "I'm a great app!!!"
   };  
-})
+});
+
+appCtrl.loadData = function($q, $timeout) {
+          var defer = $q.defer();
+          
+          $timeout(function () {
+            defer.resolve("loadData");
+          }, 2000);
+
+          return defer.promise;
+        };
+
+
+appCtrl.prepData = function($q, $timeout) {
+          var defer = $q.defer();
+          
+          $timeout(function () {
+            defer.resolve("prepData");
+          }, 2000);
+
+          return defer.promise;
+        };
+ 
 
 
 
