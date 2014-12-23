1508f01b85d575275f7ef3bd5a0b081401967c76
Chandler <maybe1046@hotmail.com>
2014-12-23 22:44:18 +1100

    41. Injectors -  Controller $Injector.invoke

--- js/main.js
+++ js/main.js
@@ -459,12 +459,11 @@ app.factory('game', function(){
   }
 });
 
-angular.injector(["app"]).invoke(function (game) {
-  alert(game.title)
-})
-
-app.controller('AppCtrl', function($scope, game){
-  $scope.title = game.title;
+app.controller('AppCtrl', function($scope, $injector){
+  $injector.invoke(function (game) {
+    $scope.title = game.title;
+    alert(game.title)
+  });
 });
 
 
